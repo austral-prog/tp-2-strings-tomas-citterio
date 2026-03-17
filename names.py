605b0b3b7ded3883a0b3d2abe2d2f31db79a7e9b
tomas-citterio <tomcitterio08@gmail.com>
2026-03-17 11:21:07 -0300

    Update names.py

--- names.py
+++ names.py
@@ -3,3 +3,9 @@ def names():
     minúsculas, título, mayúsculas y con tabulador.
     """
     pass
+    nombre = input("Ingrese el nombre: ")
+    apellido = input("Ingrese el apellido: ")
+    print(nombre.lower() + " " + apellido.lower())
+    print(nombre.title() + " " + apellido.title())
+    print(nombre.upper() + " " + apellido.upper())
+    print("\t" + nombre.lower() + " " + apellido.lower())
