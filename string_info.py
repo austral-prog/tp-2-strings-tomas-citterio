6218e9492db467fbbe9944cd5aecb76ac1c597d7
tomas-citterio <tomcitterio08@gmail.com>
2026-03-27 10:50:29 -0300

    Update string_info.py

--- string_info.py
+++ string_info.py
@@ -3,3 +3,20 @@ def string_info():
     la palabra repetida 3 veces y decorada con '***'.
     """
     palabra = "Programacion"
+    menpalabra = f"Palabra: {palabra}"
+    print(menpalabra)
+    longitud = len(palabra)
+    menlongitud = f"Longitud: {longitud}"
+    print(menlongitud)
+    pletra = palabra[0]
+    menpletra = f"Primera letra: {pletra}"
+    print(menpletra)
+    uletra = palabra[-1]
+    menuletra = f"Ultima letra: {uletra}"
+    print(menuletra)
+    repetido = palabra[ : ] * 3
+    menrepetido = f"Repetido: {repetido}"
+    print(menrepetido)
+    decoracion = ("***"+ palabra + "***")
+    mendecoracion = f"Decoracion: {decoracion}"
+    print(mendecoracion)
