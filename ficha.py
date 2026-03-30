a123b2831f9de07f55f6dfe8c2bd85cd07ecebe8
tomas-citterio <tomcitterio08@gmail.com>
2026-03-30 18:42:33 -0300

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -24,4 +24,61 @@ def ficha():
     #   - Código secreto: nombre invertido en mayúsculas
     #   - Las 3 notas, su suma, promedio y promedio entero
     #   - Cierre decorativo usando repetición de string ("=" * 24)
-    pass
+    nombre = input("Ingrese nombre completo: ")
+    email = input("Ingrese email: ")
+    nota1 = int(input("Ingrese nota 1: "))
+    nota2 = int(input("Ingrese nota 2: "))
+    nota3 = int(input("Ingrese nota 3: "))
+    print("""=====================
+FICHA DEL ALUMNO
+=====================""")
+    nombre = nombre.strip()
+    nombre = nombre.title()
+    men_nombre = f"Nombre: {nombre}"
+    print(men_nombre)
+    email = email.lower()
+    men_email = f"Email: {email}"
+    print(men_email)
+    nombrecarac = len(nombre)
+    mennombrecarac = f"Caracteres en nombre: {nombrecarac}"
+    print(mennombrecarac)
+    pos_espacio = nombre.find(" ")
+    iniciales = nombre[0] + nombre[pos_espacio + 1]
+    nombre_lower = nombre.lower()
+    meniniciales = f"-Iniciales: {iniciales}"
+    print(meniniciales)
+    nombre_usuario = nombre_lower[pos_espacio + 1:] + "." + nombre_lower[:pos_espacio]
+    menusuario = f"Usuario: {nombre_usuario}"
+    print(menusuario)
+    email_valido = "@" in email
+    menemail_valido = f"E-mail valido: {email_valido}"
+    print(menemail_valido)
+    pos_arroba = email.find("@")
+    dominio = email[pos_arroba + 1:]
+    mendominio = f"Dominio: {dominio}"
+    print(mendominio)
+    nombre_archivo = nombre_lower.replace(" ", "_")
+    menrchivo = f"Nombre para archivo: {nombre_archivo}"
+    print(menrchivo)
+    cant_a = nombre.count("a")
+    mencant_a = f"Cantidad de a: {cant_a}"
+    print(mencant_a)
+    codigo = nombre[ : :-1]
+    mencodigo = f"Codigo secreto: {codigo}"
+    print(mencodigo.upper())
+    mennota1 = f"Nota 1: {nota1}"
+    mennota2 = f"Nota 2: {nota2}"
+    mennota3 = f"Nota 3: {nota3}"
+    print(mennota1)
+    print(mennota2)
+    print(mennota3)
+    suma = nota1 + nota2 + nota3
+    mensuma = f"Suma: {suma}"
+    print(mensuma)
+    promedio_float = float((nota1 + nota2 + nota3) / 3)
+    menpromedio = f"Promedio: {promedio_float}"
+    print(menpromedio)
+    promedio_int = int((nota1 + nota2 + nota3) / 3)
+    menpromedioint = f"Promedio: {promedio_int}"
+    print(menpromedioint)
+    print("========================")
